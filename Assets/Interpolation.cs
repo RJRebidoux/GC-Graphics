1d4716d923fb8f0e30822ece3e1049235d56d812
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-25 10:01:35 -0500

    Added colour rgb and alpha lerp

--- Assets/Interpolation.cs
+++ Assets/Interpolation.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Interpolation : MonoBehaviour
 {
     public float t = 0.0f;
-    Vector3 p0 = new Vector3(-5.0f, 0.0f, 0.0f);
-    Vector3 p1 = new Vector3( 5.0f, 10.0f, 0.0f);
+    Vector3 t0 = new Vector3(-5.0f, 0.0f, 0.0f);
+    Vector3 t1 = new Vector3( 5.0f, 10.0f, 0.0f);
+    Vector3 s0 = new Vector3(1.0f, 1.0f, 1.0f);
+    Vector3 s1 = new Vector3( 2.0f, 3.0f, 0.0f);
+    Color c0 = Color.red;
+    Color c1 = Color.yellow;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +24,10 @@ public class Interpolation : MonoBehaviour
     {
         float tt = Time.realtimeSinceStartup;
         float nsin = Mathf.Sin(tt) * 0.5f + 0.5f;
-        transform.position = Vector3.Lerp(p1, p0, 0.0f);
+        transform.position = Vector3.Lerp(t0, t1, nsin);
+        transform.localScale = Vector3.Lerp(s0, s1, nsin);
+        Color color = Color.Lerp(c0, c1, nsin);
+        color.a = Mathf.Lerp(1.0f, 0.0f, nsin);
+        GetComponent<MeshRenderer>().material.color = color;
     }
 }
