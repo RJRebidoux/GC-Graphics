bb82c3c2f746f1a2a346c336919b2b751c310510
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-26 10:05:53 -0500

    Added lerp position animation

--- Assets/Interpolation.cs
+++ Assets/Interpolation.cs
@@ -8,20 +8,21 @@ public class Interpolation : MonoBehaviour
     Vector3[] original;
     Vector3 A = new Vector3(-5.0f, 0.0f);
     Vector3 B = new Vector3( 5.0f, 0.0f);
-    public float t = 0.0f;
 
     void Start()
     {
         mesh = GetComponent<MeshFilter>().mesh;
         original = mesh.vertices;
-        Debug.Log(Vector3.Lerp(A, B, t));
+
+        // We can use the computer to test our lerp calculations!
+        Debug.Log(Vector3.Lerp(A, B, 0.25f));
     }
 
     // Update is called once per frame
     void Update()
     {
         float tt = Time.realtimeSinceStartup;
-        float cos = Mathf.Cos(tt);
+        float t = Mathf.Sin(tt) * 0.5f + 0.5f;
 
         Matrix4x4 transformation = Matrix4x4.Translate(Vector3.Lerp(A, B, t));
         Vector3[] output = Transformations.Transform(transformation, original);
