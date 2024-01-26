b538e62f4b791c4f4b48cb9308e515547f7d077f
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-26 08:55:02 -0500

    Added rotation variations

--- Assets/Transformations.cs
+++ Assets/Transformations.cs
@@ -37,10 +37,28 @@ public class Transformations : MonoBehaviour
         float tt = Time.realtimeSinceStartup;
         float cos = Mathf.Cos(tt);
         Matrix4x4 scale = Matrix4x4.Scale(new Vector3(2.0f, 1.0f, 1.0f));
-        Matrix4x4 rotation = Matrix4x4.Rotate(Quaternion.Euler(0.0f, 0.0f, 45.0f));
+
+        // Matrix multiplication combines transformations, quaternion multiplication combines rotations
+        Quaternion q1 = Quaternion.Euler(tt * 100.0f, 0.0f, 0.0f);
+        Quaternion q2 = Quaternion.Euler(0.0f, 0.0f, 45.0f);
+        //Matrix4x4 rotation = Matrix4x4.Rotate(q1 * q2);
+
+        // Since matrices store rotations, we can also just combine rotation matrices!
+        Matrix4x4 r1 = Matrix4x4.Rotate(q1);
+        Matrix4x4 r2 = Matrix4x4.Rotate(q2);
+        //Matrix4x4 rotation = r1 * r2;
+
+        // (q1 * q2 is unnecessary because q1 is x and q2 is z so we could make this into a single quaternion initially)
+        Matrix4x4 rotation = Matrix4x4.Rotate(Quaternion.Euler(tt * 100.0f, 0.0f, 45.0f));
+
         Matrix4x4 translation = Matrix4x4.Translate(new Vector3(5.0f * cos, 0.0f, 0.0f));
         //Matrix4x4 result = translation;// * rotation * scale;
         //Matrix4x4 result = translation * rotation * scale;
+
+        // Default matrix is a zero-matrix which will cause our vertices to be zero!
+        //Matrix4x4 transformation = new Matrix4x4();
+        Matrix4x4 transformation = rotation * Matrix4x4.identity;
+        Debug.Log(transformation);
 
         // ORDER MATTERS!!! Matrices are multiplied right-to-left, so the right-most matrix will be applied first.
         // For example, if we rotate then translate, the rotation will be done relative to the world origin,
@@ -51,6 +69,7 @@ public class Transformations : MonoBehaviour
         //Vector3[] newVertices = Transform(rotation * translation, original); <-- translate then rotate (unexpected result)
 
         // Generally, the order is scale then rotate then translate
-        mesh.vertices = Transform(translation * rotation * scale, original);
+        Vector3[] output = Transform(transformation, original);
+        mesh.vertices = output;
     }
 }
