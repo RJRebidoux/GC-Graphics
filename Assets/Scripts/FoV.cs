7dbe8137ca97e52dacc2d6c174d0c1e1b88e787a
Smiley98 <smileysofficialcinema@gmail.com>
2024-04-04 09:20:29 -0400

    Added FoV calculation

--- Assets/Scripts/FoV.cs
+++ Assets/Scripts/FoV.cs
@@ -12,7 +12,16 @@ public class FoV : MonoBehaviour
         Vector3 targetDirection = (targetPosition - viewPosition).normalized;
         Debug.DrawLine(viewPosition, viewPosition + viewDirection * 5.0f, Color.green);
         Debug.DrawLine(viewPosition, viewPosition + targetDirection * 5.0f, Color.red);
-        return false;
+
+        // Compare view direction (red in spotlight diagram) with our view direction (black in spotlight diagram)
+        float dot = Vector3.Dot(viewDirection, targetDirection);
+
+        // Dot(a, b) = |a| x |b| x cos(t)
+        // We know a & b are unit-vectors, so their magnitudes are 1.
+        // 1 x 1 x cos(t) = cos(t) --> we can remove the magnitudes from our equation!
+        // Dot(a, b) = cos(t)
+
+        return Mathf.Cos(angle * 0.5f * Mathf.Deg2Rad) < dot;
     }
 
     float angle = 60.0f;
@@ -31,7 +40,8 @@ public class FoV : MonoBehaviour
         float nsin = Mathf.Sin(tt) * 0.5f + 0.5f;
         transform.rotation = Quaternion.Slerp(r0, r1, nsin);
 
-        InFoV(transform.position, target.position, transform.up, angle);
+        bool inFoV = InFoV(transform.position, target.position, transform.up, angle);
+        target.GetComponent<MeshRenderer>().material.color = inFoV ? Color.red : Color.green;
         //Debug.DrawLine(transform.position, transform.position + transform.up * 5.0f, Color.red);
     }
 }
